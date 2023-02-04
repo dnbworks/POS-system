be60d951c8855a2f8aa2cfaba50f3d8972d1ad26
dnbworks <dooyongnsaakobalderama@gmail.com>
2023-02-04 22:56:06 +0800

    add report dir

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom'; 
 import { AppProvider } from './context/AppContext'
 
+// import { store } from './app/store';
+// import { Provider } from 'react-redux';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/views/LoginPage.js
+++ src/views/LoginPage.js
@@ -2,16 +2,22 @@ import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div className='container'>
+    <div className='container login'>
       <h3>Login System</h3>
       <form >
-        <label htmlFor="username">Username</label>
-        <input type="text" />
-        <label htmlFor="password">Password</label>
-        <input type="password" />
+        <div className='input-field'>
+          <label htmlFor="username">Username</label>
+          <input type="text" />
+        </div>
+        <div className='input-field'>
+          <label htmlFor="password">Password</label>
+          <input type="password" />
+        </div>
         <a href="/">Forgot password</a>
-        <button>Login</button>
-        <button>Cancel</button>
+        <div className='d-flex justify-content-right'>
+          <button>Login</button>
+          <button>Cancel</button>
+        </div>
       </form>
     </div>
   )
